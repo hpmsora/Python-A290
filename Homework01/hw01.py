ac2f2aa394a83cd19e3d45188d594976bb5a748b
Won Yong Ha <Owl@140-182-198-234.dhcp-bl.indiana.edu>
2016-09-08 16:33:33 -0400

    8 Sep 2016

--- Homework01/hw01.py
+++ Homework01/hw01.py
@@ -1,6 +1,30 @@
 #Multiplication table
 
+import random
+
+#Homework 1 - 1
 for i in range(1, 11):
     for j in range (1, 11):
         print(str(i * j), end='\t')
     print("\n")
+print('\n')
+
+#Homework 1 - 2
+def hw1_2():
+    ans = random.randrange(100)
+    attempts = 0
+
+    while True:
+        attempts += 1
+        i = input('Your guess?: ')
+        i = int(i)
+        if i == ans:
+            print('Correct!')
+            print('You tried ' + str(attempts) + ' times')
+            break
+        elif i > ans:
+            print('Lower')
+        else:
+            print('Higher')
+
+hw1_2()
